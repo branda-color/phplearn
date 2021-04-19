524b6f9dae0e6d0b4838c8bb1e1813b371fcd36a
tzu <wishcolor0770@yahoo.com.tw>
2021-04-19 16:57:35 +0800

    查詢數據

--- phpsyntax/mongodb.php
+++ phpsyntax/mongodb.php
@@ -1,17 +1,31 @@
 <?php
  
-
+//連接mongodb
  $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 
- $bulk = new MongoDB\Driver\BulkWrite;
- $document = ['_id' => new MongoDB\BSON\ObjectID, 'name' => '測試連線'];
- 
- $_id= $bulk->insert($document);
- 
- var_dump($_id);
- 
- $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");  
- $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
- $result = $manager->executeBulkWrite('test.yuyu', $bulk, $writeConcern);
+ // 插入数据
+$bulk = new MongoDB\Driver\BulkWrite;
+$bulk->insert(['x' => 1, 'name'=>'菜鸟教程', 'url' => 'http://www.runoob.com']);
+$bulk->insert(['x' => 2, 'name'=>'Google', 'url' => 'http://www.google.com']);
+$bulk->insert(['x' => 3, 'name'=>'taobao', 'url' => 'http://www.taobao.com']);
+//$manager->executeBulkWrite('test.sites', $bulk);
+
+
+//從第幾個開始查詢
+$filter = ['x' => ['$gt' => 0]];
+
+//排列情況
+$options = [
+    'projection' => ['_id' => 0],//不顯示id所以設為0
+    'sort' => ['x' => -1],//降冪排列
+];
+
+// 查询数据
+$query = new MongoDB\Driver\Query($filter, $options);
+$cursor = $manager->executeQuery('test.sites', $query);
+
+foreach ($cursor as $document) {
+    print_r($document);
+}
 
 
